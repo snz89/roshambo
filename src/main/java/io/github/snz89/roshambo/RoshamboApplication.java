5e99d6616e419b13fd53fd9546ea781d89656512
snz <seanzfx@yandex.ru>
2026-07-28 12:48:32 +0300

    add token keys in configuration

--- src/main/java/io/github/snz89/roshambo/RoshamboApplication.java
+++ src/main/java/io/github/snz89/roshambo/RoshamboApplication.java
@@ -2,8 +2,10 @@ package io.github.snz89.roshambo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class RoshamboApplication {
 
 	public static void main(String[] args) {
